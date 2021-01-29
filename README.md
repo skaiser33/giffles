760c50b820933fb61694fbf65a18c64fcf2bd6fa
skaiser33 <skaiser33@gmail.com>
2021-01-29 16:29:21 -0500

    iniitial setup

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # project1_api_dashboard
+
+REQUIREMENTS
+ An embedded screenshot of the app
+ List of the Technologies used
+ Installation instructions
+ Your User stories – who are your users, what do they want and why?
+ Your Wireframes – sketches of major views / interfaces in your application
+ Descriptions of any Unsolved problems or major hurdles you had to overcome
---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+const chuckCategory = document.querySelector("select");
+const chuckButton = document.getElementById("chuck");
+
+
+const requestUrl = "https://api.giphy.com/v1/gifs/search?api_key=C45MMlNyrdjBOB9vgOy9BkNBfEhE4UOb&q=queen&limit=1&offset=0&rating=g&lang=en";
+
+
+chuckButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    fetch(`${requestUrl}`)
+    .then((response) => {
+        return response.json();
+    })
+    .then((parsedData) => {
+        console.log("======>", parsedData.data[0].embed_url);
+        document.querySelector("#testing").src = parsedData.data[0].embed_url;
+
+        // <iframe src="https://giphy.com/embed/l1KVcMMxJJpks23cs" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/thewhiteprincess-season-1-l1KVcMMxJJpks23cs">via GIPHY</a></p>
+    })
+    .catch((error) => {
+        console.error("ERROR: ", error)
+    });
+})

1dd11e96a4536c613e25699550b8a20283e2a938
skaiser33 <skaiser33@gmail.com>
2021-02-01 17:56:33 -0500

    adjusts iframes for # of words in title

--- js/main.js
+++ js/main.js
@@ -33,30 +33,34 @@
 
 //TIMER? Modify scoring
 //Sound for correct answer? Funny gif to make fun of player if they get the wrong answer and celebratory gif if right.
+//Avoid repeats in same game? by modifying all arrays (in which case i'd need to reset each new player) or better solution available?
 
-//ARRAYS
-//COMPLETE: RANDOMIZE CHOICE OF PULL FROM SOURCEARRAY
 
-//GIFs
-//COMPLETE: AUTO RESIZE or FIXED HEIGHT? -- due to embed properties in API, best to just set the same height/width for all iframes and the embedded gif will conform its largest dimension accordingly. 
 
+
+
+//SCHEDULE:
 // WED:
-// Styling
+// Styling (start with 3 blank iframes...maybe have a different colored ? in each)
 
 // TUE:
+
 // Array creation and separate array storage
+// Solve BenjamIN button issue
 // Begin styling
 
 //MONDAY: 
+//Update new clue button to act according to title length
 //Use length of title to display number of iframes. Rather than hiding, should I DOM this? 
  //Collapsible instructions (refer to pre-work)
 // New Player / Reset button? [just reload website or be more sophisticated?]
 
 
 //Array filtering and mapping
-const moviesRawArray = ["Fight Club", "Hard Day's Night", "Fantastic Mr. Fox", "Muppets Take Manhattan", "Being John Malkovich", "Fight Club", "The Unbearable Lightness Of Being", "The Curious Case Of Benjamin Button"];
-const countWords = (rawTitle) => {return rawTitle.split(" ").length >= 3};
-const moviesMasterArray = moviesRawArray.filter(countWords);
+const moviesMasterArray = ["Adaptation", "Fight Club", "Panic Room", "Boogie Nights", "Hard Day's Night", "Fantastic Mr. Fox", "Muppets Take Manhattan", "Being John Malkovich", "The Unbearable Lightness Of Being", "No Country For Old Men", "There Will Be Blood", "Die Hard With A Vengeance","Monty Python and the Quest for the Holy Grail"];
+
+//const countWords = (rawTitle) => {return rawTitle.split(" ").length >= 3}; //THIS WAS AN INITIAL CONSTRAINT FOR TESTING
+//const moviesMasterArray = moviesRawArray.filter(countWords);  //THIS WAS AN INITIAL CONSTRAINT FOR TESTING
 const eliminateWords = (masterTitle) => {return masterTitle.replace(/a |an |are |in |is |of |the /gi, "")};
 const moviesSourceArray = moviesMasterArray.map(eliminateWords);
 
@@ -71,9 +75,19 @@ const answerInput = document.getElementById("answer");
 const isPlayerCorrect = document.getElementById("is-player-correct");
 
 //Variable declarations
-let randomMovieIndex, titleLength, firstWord, secondWord, thirdWord, requestFirstUrl, requestSecondUrl, requestThirdUrl, gif1Array, gif2Array, gif3Array, guess
-let g = 0; // TO-DO variable for array index that chooses round of gif's; will need to increment with each Push of "new clues button and reset with each new round
+let randomMovieIndex, firstWord, secondWord, thirdWord, fourthWord, requestFirstUrl, requestSecondUrl, requestThirdUrl, gif1Array, gif2Array, gif3Array, gif4array, guess
+let g = 0; // variable for array index that chooses round of gif's; will need to increment with each Push of "new clues button and reset with each new round
 let pScore = 0;
+let titleLength = 0;
+
+//Functions
+const getTitle = () => {
+    randomMovieIndex = Math.floor(Math.random() * moviesSourceArray.length)
+    console.log(moviesSourceArray[randomMovieIndex]) // shows the mapped title
+    //splits mapped title into individual words
+    titleLength = moviesSourceArray[randomMovieIndex].split(" ").length;
+    console.log(titleLength);
+}
 
 // Create Score Container
 let scoreContainer = document.createElement('div');
@@ -98,17 +112,18 @@ nextButton.addEventListener("click", (e) => {
     isPlayerCorrect.classList.toggle("hide");  
     nextRoundForm.classList.toggle("hide");
     //Chooses random title
-    randomMovieIndex = Math.floor(Math.random() * moviesSourceArray.length)
-    console.log(moviesSourceArray[randomMovieIndex]) // shows the mapped title
-    //splits mapped title into individual words
-    titleLength - moviesSourceArray[randomMovieIndex].split(" ").length;
-    firstWord = moviesSourceArray[randomMovieIndex].split(" ")[0];
-    secondWord = moviesSourceArray[randomMovieIndex].split(" ")[1];
-    thirdWord = moviesSourceArray[randomMovieIndex].split(" ")[2];
-    requestFirstUrl = `https://api.giphy.com/v1/gifs/search?api_key=C45MMlNyrdjBOB9vgOy9BkNBfEhE4UOb&q=${ firstWord }&limit=5&offset=0&lang=en`
-    requestSecondUrl = `https://api.giphy.com/v1/gifs/search?api_key=C45MMlNyrdjBOB9vgOy9BkNBfEhE4UOb&q=${ secondWord }&limit=5&offset=0&lang=en`
-    requestThirdUrl = `https://api.giphy.com/v1/gifs/search?api_key=C45MMlNyrdjBOB9vgOy9BkNBfEhE4UOb&q=${ thirdWord }&limit=5&offset=0&lang=en`
+    getTitle();
+    //If title length is too short / too long, pick another movie
+    while (titleLength < 2 || titleLength > 4) {
+        getTitle();
+    }
     
+    //For two words, hide 3rd iframe and spread out the first two
+    //For four words, toggle class "giphy-embed" and condense all of them
+
+
+    firstWord = moviesSourceArray[randomMovieIndex].split(" ")[0];
+    requestFirstUrl = `https://api.giphy.com/v1/gifs/search?api_key=C45MMlNyrdjBOB9vgOy9BkNBfEhE4UOb&q=${ firstWord }&limit=5&offset=0&lang=en`
     //Fetch request from giphy to provide gif for 1st word 
     fetch(`${ requestFirstUrl }`)
     .then((response) => {
@@ -123,6 +138,8 @@ nextButton.addEventListener("click", (e) => {
         console.error("ERROR: ", error)
     });
 
+    secondWord = moviesSourceArray[randomMovieIndex].split(" ")[1];
+    requestSecondUrl = `https://api.giphy.com/v1/gifs/search?api_key=C45MMlNyrdjBOB9vgOy9BkNBfEhE4UOb&q=${ secondWord }&limit=5&offset=0&lang=en`
     //Fetch request from giphy to provide gif for 2nd word 
     fetch(`${ requestSecondUrl }`)
     .then((response) => {
@@ -137,19 +154,53 @@ nextButton.addEventListener("click", (e) => {
         console.error("ERROR: ", error)
     });
     
-    //Fetch request from giphy to provide gif for 3rd word 
-    fetch(`${ requestThirdUrl }`)
-    .then((response) => {
-        return response.json();
-    })
-    //Feed gif #three embed url into iframe #three
-    .then((parsedData) => {
-        gif3Array = parsedData.data;
-        document.querySelector("#three").src = gif3Array[g].embed_url;
-    })
-    .catch((error) => {
-        console.error("ERROR: ", error)
-    });
+    if (titleLength >= 3) {
+        document.querySelector("#three").classList.remove("hide");
+        thirdWord = moviesSourceArray[randomMovieIndex].split(" ")[2];
+        requestThirdUrl = `https://api.giphy.com/v1/gifs/search?api_key=C45MMlNyrdjBOB9vgOy9BkNBfEhE4UOb&q=${ thirdWord }&limit=5&offset=0&lang=en`
+        //Fetch request from giphy to provide gif for 3rd word 
+        fetch(`${ requestThirdUrl }`)
+        .then((response) => {
+            return response.json();
+        })
+        //Feed gif #three embed url into iframe #three
+        .then((parsedData) => {
+            gif3Array = parsedData.data;
+            document.querySelector("#three").src = gif3Array[g].embed_url;
+        })
+        .catch((error) => {
+            console.error("ERROR: ", error)
+        });
+    } else { 
+        document.querySelector("#three").classList.add("hide");
+        document.querySelector("#three").src = "";
+        thirdWord = "" 
+    }    
+
+    if (titleLength === 4) {
+        document.querySelector("#four").classList.remove("hide");
+        document.querySelector("#four").classList.add("giphy-embed");
+        fourthWord = moviesSourceArray[randomMovieIndex].split(" ")[3];
+        requestFourthUrl = `https://api.giphy.com/v1/gifs/search?api_key=C45MMlNyrdjBOB9vgOy9BkNBfEhE4UOb&q=${ fourthWord }&limit=5&offset=0&lang=en`
+        //Fetch request from giphy to provide gif for 3rd word 
+        fetch(`${ requestFourthUrl }`)
+        .then((response) => {
+            return response.json();
+        })
+        //Feed gif #three embed url into iframe #three
+        .then((parsedData) => {
+            gif4Array = parsedData.data;
+            document.querySelector("#four").src = gif4Array[g].embed_url;
+        })
+        .catch((error) => {
+            console.error("ERROR: ", error)
+        });
+    } else { 
+        document.querySelector("#four").classList.remove("giphy-embed");
+        document.querySelector("#four").classList.add("hide");
+        document.querySelector("#four").src = "";
+        fourthWord = "" 
+    }    
 })
 
 //new clues event listener
@@ -186,3 +237,9 @@ answerButton.addEventListener("click", (e) => {
 
 
 
+//COMPLETED:
+//ARRAYS
+//COMPLETE: RANDOMIZE CHOICE OF PULL FROM SOURCEARRAY
+
+//GIFs
+//COMPLETE: AUTO RESIZE or FIXED HEIGHT? -- due to embed properties in API, best to just set the same height/width for all iframes and the embedded gif will conform its largest dimension accordingly. 
